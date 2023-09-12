b08bcd14ac50de07446d4a07df34e3ccdfe98791
jungcali94 <jungcali94@gmail.com>
2023-09-12 17:49:36 +0900

    init project

---
+++ src/main/kotlin/hanghae99/plus2/hanghae/HanghaeApplication.kt
@@ -0,0 +1,11 @@
+package hanghae99.plus2.hanghae
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HanghaeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HanghaeApplication>(*args)
+}
---
+++ src/test/kotlin/hanghae99/plus2/hanghae/HanghaeApplicationTests.kt
@@ -0,0 +1,13 @@
+package hanghae99.plus2.hanghae
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HanghaeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
