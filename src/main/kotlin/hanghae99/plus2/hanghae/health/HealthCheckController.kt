7639892b872d516c3994667ce598eb46b0d847ac
macCali <jungcali94@gmail.com>
2023-09-15 22:17:33 +0900

    오류 수정

--- src/main/kotlin/hanghae99/plus2/hanghae/health/HealthCheckController.kt
+++ src/main/kotlin/hanghae99/plus2/hanghae/health/HealthCheckController.kt
@@ -1,13 +1,12 @@
-package hanghae99.plus2.team3.health
+package hanghae99.plus2.hanghae.health
 
-import hanghae99.plus2.hanghae.health.HealthService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/api")
 @RestController
-class CicdController(
+class HealthCheckController(
     private val healthService: HealthService
 ) {
 
