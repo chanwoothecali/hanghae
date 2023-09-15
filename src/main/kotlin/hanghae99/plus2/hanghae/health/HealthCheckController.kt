3d42a434b6fb5900c70148bba70a60f91ee26ef7
macCali <jungcali94@gmail.com>
2023-09-15 20:56:30 +0900

    controller 수정

--- src/main/kotlin/hanghae99/plus2/hanghae/health/HealthCheckController.kt
+++ src/main/kotlin/hanghae99/plus2/hanghae/health/HealthCheckController.kt
@@ -1,5 +1,6 @@
 package hanghae99.plus2.team3.health
 
+import hanghae99.plus2.hanghae.health.HealthService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 @RestController
 class CicdController(
-    private val cicdService: CicdService
+    private val healthService: HealthService
 ) {
 
     @GetMapping("/health")
     fun healthCheck(): String {
         return "ok"
+    }
+
+    @GetMapping("/something")
+    fun something(): String {
+        return healthService.doSomething()
     }
 }
