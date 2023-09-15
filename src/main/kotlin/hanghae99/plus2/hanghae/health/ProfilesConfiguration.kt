aa6c5233403f8721c9bafbb3ad5a5ca689f0fb8a
macCali <jungcali94@gmail.com>
2023-09-15 22:22:04 +0900

    configuration 작성

--- src/main/kotlin/hanghae99/plus2/hanghae/health/ProfilesConfiguration.kt
+++ src/main/kotlin/hanghae99/plus2/hanghae/health/ProfilesConfiguration.kt
@@ -1,8 +1,10 @@
 package hanghae99.plus2.hanghae.health
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
+@Configuration
 class ProfilesConfiguration {
 
     @Profile("test")
